524e6a4dbb20e114ca2bd942ee0f02721d17bde3
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-10-26 15:57:09 -0500

    Git tree test with data returned

--- test/repo-man.xunit/domain/GitRepositoryTreeExtracterTest.cs
+++ test/repo-man.xunit/domain/GitRepositoryTreeExtracterTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
 using Moq.AutoMock;
 using repo_man.domain.Git;
 
@@ -19,6 +20,29 @@ namespace repo_man.xunit.domain
             result.Should().NotBeNull();
             result.Files.Should().BeEmpty();
             result.Folders.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void AllGitFilesReturnedAreStoredInTree()
+        {
+            var fixture = new Fixture();
+            var expectedLengths = fixture.CreateMany<long>(2).ToArray();
+            var mocker = new AutoMocker();
+            mocker.GetMock<IGitRepoCrawler>().Setup(x => x.GetFilesWithCommits())
+                .Returns(new[]
+                {
+                    ("readme.md", expectedLengths[0], new Commit[0]),
+                    ("kung/fu.json", expectedLengths[1], new Commit[0])
+                }.AsEnumerable());
+
+            var target = mocker.CreateInstance<GitRepositoryTreeExtracter>();
+            var result = target.GetFileTree();
+
+            result.Should().NotBeNull();
+            result.Files.Single().Should().BeEquivalentTo(new GitFile("readme.md", expectedLengths[0], new Commit[0]));
+            result.Folders.Single().Name.Should().Be("kung");
+            result.Folders.Single().Files.Single().Should()
+                .BeEquivalentTo(new GitFile("fu.json", expectedLengths[1], new Commit[0]));
         }
     }
 }
