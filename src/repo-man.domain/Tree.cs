e320f6b6bf192c4d1010ecb386b0708651f6d9df
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-10-26 14:57:18 -0500

    adding folders to Tree

--- src/repo-man.domain/Tree.cs
+++ src/repo-man.domain/Tree.cs
@@ -1,14 +1,79 @@
-﻿using repo_man.domain.Git;
+﻿using System.Xml.Serialization;
+using repo_man.domain.Git;
 
 namespace repo_man.domain;
 
 public class Tree
 {
-    public void AddFile(string filePath, long FileSize, Commit[] commits)
+    public void AddFile(string filePath, long fileSize, Commit[] commits)
     {
-        _topLevelFiles.Add(new GitFile(filePath, FileSize, commits.AsReadOnly()));
+        if (filePath.Contains("/"))
+        {
+            var segments = filePath.Split('/');
+            var folderName = segments[0];
+            Func<GitFolder, bool> matchingName = f => f.Name == folderName;
+
+            if (!Folders.Any(matchingName))
+            {
+                AddFolder(folderName);
+            }
+            Folders.First(matchingName)
+                .AddFile(segments.Skip(1).ToArray(), fileSize, commits);
+        }
+        else
+        {
+            _topLevelFiles.Add(new GitFile(filePath, fileSize, commits.AsReadOnly()));
+        }
+    }
+
+    private void AddFolder(string folderName)
+    {
+        _folders.Add(new GitFolder(folderName));
     }
 
     private readonly List<GitFile> _topLevelFiles = new List<GitFile>();
     public IReadOnlyCollection<GitFile> TopLevelFiles => _topLevelFiles.AsReadOnly();
+
+    private readonly List<GitFolder> _folders = new List<GitFolder>();
+    public IReadOnlyCollection<GitFolder> Folders => _folders.AsReadOnly();
+}
+
+public class GitFolder
+{
+    public string Name { get; }
+
+    public GitFolder(string name)
+    {
+        Name = name;
+    }
+
+    private readonly List<GitFile> _files = new List<GitFile>();
+    public IReadOnlyCollection<GitFile> Files => _files.AsReadOnly();
+
+    private readonly List<GitFolder> _folders = new List<GitFolder>();
+    public IReadOnlyCollection<GitFolder> Folders => _folders.AsReadOnly();
+
+    public void AddFile(string[] segments, long fileSize, Commit[] commits)
+    {
+        if (segments.Length == 1)
+        {
+            _files.Add(new GitFile(segments[0], fileSize, commits.AsReadOnly()));
+        }
+        else if (segments.Length > 1)
+        {
+            var folderName = segments[0];
+            Func<GitFolder, bool> matchingName = f => f.Name == folderName;
+            if (!Folders.Any(matchingName))
+            {
+                AddFolder(folderName);
+            }
+            Folders.First(matchingName)
+                .AddFile(segments.Skip(1).ToArray(), fileSize, commits);
+
+        }
+    }
+    private void AddFolder(string folderName)
+    {
+        _folders.Add(new GitFolder(folderName));
+    }
 }
--- test/repo-man.xunit/domain/TreeTest.cs
+++ test/repo-man.xunit/domain/TreeTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture;
+using FluentAssertions;
 using repo_man.domain;
 using repo_man.domain.Git;
 
@@ -9,14 +10,34 @@ namespace repo_man.xunit.domain
         [Fact]
         public void TopLevelFileSavedToTree()
         {
-            var target = new Tree();
-            var expectedHash = "abcdefghiasd";
-            target.AddFile("readme.md", long.Parse("123456"),  new[] { new Commit(expectedHash, DateTimeOffset.Now) });
+            //arrange
+            var fixture = new Fixture();
+            var expectedHash = fixture.Create<string>();
+            var expectedDate = DateTimeOffset.Now;
 
-            target.TopLevelFiles.Single().Commits.Single().Hash.Should().Be(expectedHash);
-            target.TopLevelFiles.Single().Commits.Single().CommitDate.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromSeconds(5.0));
+            //act
+            var target = new Tree();
+            target.AddFile("readme.md", fixture.Create<long>(),  new[] { new Commit(expectedHash, expectedDate) });
+
+            //assert
+            target.TopLevelFiles.Single().Commits.Single().Should()
+                .BeEquivalentTo(new Commit(expectedHash, expectedDate));
         }
 
+        [Fact]
+        public void FileFromTopLevelFolderStoredLowerInTree()
+        {
+            //arrange
+            var fixture = new Fixture();
 
+            //act
+            var target = new Tree();
+            target.AddFile(".config/dotnet-tools.json", fixture.Create<long>(), new []{ new Commit(fixture.Create<string>(), DateTimeOffset.Now)});
+
+            target.TopLevelFiles.Should().BeEmpty();
+            var folder = target.Folders.Single();
+            folder.Name.Should().Be(".config");
+            folder.Files.Single().Name.Should().Be("dotnet-tools.json");
+        }
     }
 }
