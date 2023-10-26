cbcda538ec880d3536fe17e71503f6e350ebc1ea
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-10-26 12:23:43 -0500

    first unit test

---
+++ test/repo-man.xunit/_extensions/MoqLoggerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repo_man.xunit._extensions
+{
+    internal static class MoqLoggerExtensions
+    {
+        internal static Mock<ILogger<T>> VerifyErrorWasCalled<T>(this Mock<ILogger<T>> logger, string expectedMessage, Times times)
+        {
+            Func<object, Type, bool> state = (v, t) => String.Compare(v.ToString(), expectedMessage, StringComparison.Ordinal) == 0;
+
+            logger.Verify(
+                x => x.Log(
+                    LogLevel.Error,
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), times);
+
+            return logger;
+        }
+    }
+}
---
+++ test/repo-man.xunit/domain/RepositoryVisualizerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.AutoMock;
+using repo_man.xunit._extensions;
+
+namespace repo_man.xunit.domain
+{
+    public class RepositoryVisualizerTest
+    {
+        [Fact]
+        public async Task ExitsEarlyIfRepoPathCannotBeFound()
+        {
+            var mocker = new AutoMocker();
+            var target = mocker.CreateInstance<RepositoryVisualizer>();
+
+            await target.GenerateDiagram();
+
+            mocker.GetMock<ILogger<RepositoryVisualizer>>()
+                .VerifyErrorWasCalled("Repository path has not been specified. Exiting.", Times.Once());
+        }
+}
+
+    public class RepositoryVisualizer
+    {
+        private readonly ILogger<RepositoryVisualizer> _logger;
+
+        public RepositoryVisualizer(ILogger<RepositoryVisualizer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task GenerateDiagram()
+        {
+            _logger.LogError("Repository path has not been specified. Exiting.");
+            return Task.CompletedTask;
+        }
+    }
+}
