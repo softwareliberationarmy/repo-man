16c2cd3ad8d4a86271e5b687bc5d8793c96a4875
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-10-26 15:13:26 -0500

    adding deeply nested test

--- test/repo-man.xunit/domain/TreeTest.cs
+++ test/repo-man.xunit/domain/TreeTest.cs
@@ -7,17 +7,23 @@ namespace repo_man.xunit.domain
 {
     public class TreeTest
     {
+        private readonly Fixture _fixture;
+
+        public TreeTest()
+        {
+            _fixture = new Fixture();
+        }
+
         [Fact]
         public void TopLevelFileSavedToTree()
         {
             //arrange
-            var fixture = new Fixture();
-            var expectedHash = fixture.Create<string>();
+            var expectedHash = _fixture.Create<string>();
             var expectedDate = DateTimeOffset.Now;
 
             //act
             var target = new Tree();
-            target.AddFile("readme.md", fixture.Create<long>(),  new[] { new Commit(expectedHash, expectedDate) });
+            target.AddFile("readme.md", _fixture.Create<long>(),  new[] { new Commit(expectedHash, expectedDate) });
 
             //assert
             target.Files.Single().Commits.Single().Should()
@@ -28,16 +34,31 @@ namespace repo_man.xunit.domain
         public void FileFromTopLevelFolderStoredLowerInTree()
         {
             //arrange
-            var fixture = new Fixture();
 
             //act
             var target = new Tree();
-            target.AddFile(".config/dotnet-tools.json", fixture.Create<long>(), new []{ new Commit(fixture.Create<string>(), DateTimeOffset.Now)});
+            target.AddFile(".config/dotnet-tools.json", _fixture.Create<long>(), new []{ new Commit(_fixture.Create<string>(), DateTimeOffset.Now)});
 
             target.Files.Should().BeEmpty();
             var folder = target.Folders.Single();
             folder.Name.Should().Be(".config");
             folder.Files.Single().Name.Should().Be("dotnet-tools.json");
+        }
+
+        [Fact]
+        public void ItsTurtlesAllTheWayDown()
+        {
+            var target = new Tree();
+            target.AddFile("any/way/you/want/it/thats/the/way/you/need/it.txt", _fixture.Create<long>(), new []{ _fixture.Create<Commit>()});
+
+            var expectedFolders = new[] { "any", "way", "you", "want", "it", "thats", "the", "way", "you", "need" };
+
+            var currentFolders = target.Folders;
+            for (int i = 0; i < expectedFolders.Length; i++)
+            {
+                currentFolders.Single().Name.Should().Be(expectedFolders[i]);
+                currentFolders = currentFolders.Single().Folders;
+            }
         }
     }
 }
