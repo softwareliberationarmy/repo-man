db28979328d8ed4b57ac3aaebd7f9995d0da3085
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-10-27 08:42:00 -0500

    Implementing libgit2sharp repo crawler

--- src/repo-man.infrastructure/LibGit2SharpGitRepoCrawler.cs
+++ src/repo-man.infrastructure/LibGit2SharpGitRepoCrawler.cs
@@ -1,17 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibGit2Sharp;
+using Microsoft.Extensions.Configuration;
 using repo_man.domain.Git;
+using Commit = repo_man.domain.Git.Commit;
 
 namespace repo_man.infrastructure
 {
     public class LibGit2SharpGitRepoCrawler: IGitRepoCrawler
     {
+        private readonly IConfiguration _config;
+
+        public LibGit2SharpGitRepoCrawler(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public IEnumerable<(string, long, Commit[])> GetFilesWithCommits()
         {
-            throw new NotImplementedException();
+            var repoPath = _config["repo"];
+            if (string.IsNullOrEmpty(repoPath))
+            {
+                throw new InvalidOperationException("Repo path not specified.");
+            }
+
+            using var repo = new Repository(repoPath);
+            var tree = repo.Head.Tip.Tree;
+
+            return ParseTree(tree, repo);
+        }
+
+        private IEnumerable<(string, long, Commit[])> ParseTree(Tree tree, Repository? repo)
+        {
+            var result = new List<(string, long, Commit[])>();
+
+            foreach (var entry in tree)
+            {
+                if (entry.TargetType == TreeEntryTargetType.Tree)
+                {
+                    foreach (var valueTuple in ParseTree((Tree)entry.Target, repo))
+                    {
+                        result.Add(valueTuple);
+                    }
+                }
+                else if (entry.TargetType == TreeEntryTargetType.Blob)
+                {
+                    var history = repo!.Commits.QueryBy(entry.Path).Select(
+                        c => new Commit(c.Commit.Sha, c.Commit.Committer.When)).ToArray();
+                    var size = ((Blob)entry.Target).Size;
+
+                    result.Add((entry.Path, size, history));
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ test/repo-man.xunit/infrastructure/LibGit2SharpGitRepoCrawlerTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq.AutoMock;
+using repo_man.infrastructure;
+
+namespace repo_man.xunit.infrastructure
+{
+    public class LibGit2SharpGitRepoCrawlerTest
+    {
+        [Fact]
+        public void ThrowsExceptionWhenRepoPathNotConfigured()
+        {
+            var mocker = new AutoMocker();
+            mocker.GetMock<IConfiguration>().Setup(c => c["repo"]).Returns((string?)null);
+
+            var target = mocker.CreateInstance<LibGit2SharpGitRepoCrawler>();
+
+            Action getFiles = () => { target.GetFilesWithCommits(); };
+            getFiles.Should().Throw<InvalidOperationException>();
+        }
+
+
+    }
+}
