2e5d33e6341141e431ba5dde151933bcede4e41e
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-11-04 15:29:24 -0500

    moving todo list to readme file

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # repo-man
 
 A console application that generates diagrams helping users to visualize the codebase
+
+## TODO
+* implement IFileColorMapper
+* define SvgLegendDataWriter
+* extract interface from SvgChartDataWriter
+* test the composition of the SvgDiagramRenderer now that we know where we're going with this
+* add infrastructure class to write data to file (thin wrapper)
+* define SvgComposer or use SvgDiagramRenderer as composer and add an SvgBookendManager
+* add logging to the SvgChartDataWriter
+* add logging to LibGit2SharpGitRepoCrawler
+* add logging to GitRepositoryTreeExtracter
+* feature: user can specify background color
--- src/repo-man.console/Program.cs
+++ src/repo-man.console/Program.cs
@@ -4,15 +4,3 @@ using repo_man.domain;
 var visualizer = Bootstrapper.InitializeToTopLevelService<RepositoryVisualizer>(args);
 await visualizer.GenerateDiagram();
 
-
-//TO DO LIST: 
-// implement IFileColorMapper
-// define SvgLegendDataWriter
-// extract interface from SvgChartDataWriter
-// test the composition of the SvgDiagramRenderer now that we know where we're going with this
-// add infrastructure class to write data to file (thin wrapper)
-// define SvgComposer or use SvgDiagramRenderer as composer and add an SvgBookendManager
-// add logging to the SvgChartDataWriter
-// add logging to LibGit2SharpGitRepoCrawler
-// add logging to GitRepositoryTreeExtracter
-// feature: user can specify background color
