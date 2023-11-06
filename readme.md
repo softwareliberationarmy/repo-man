4d06ce413abcfe928731140d3922a860f11cc9e4
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-11-06 08:09:57 -0600

    Starting unit tests for constant file color mapper

--- readme.md
+++ readme.md
@@ -3,10 +3,12 @@
 A console application that generates diagrams helping users to visualize the codebase
 
 ## TODO
-* implement IFileColorMapper
+=> implement IFileColorMapper
+* add logic for files with no extension to return the full file name
 * define SvgLegendDataWriter
 * test the composition of the SvgDiagramRenderer now that we know where we're going with this
 * add infrastructure class to write data to file (thin wrapper)
 * define SvgComposer or use SvgDiagramRenderer as composer and add an SvgBookendManager
 * add logging to the SvgChartDataWriter
 * feature: user can specify background color
+* feature: implement ignored file extensions for binary file types (png, jpg, bmp, etc.)
---
+++ test/repo-man.xunit/domain/Diagram/ConstantFileColorMapperTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Moq.AutoMock;
+using repo_man.domain.Diagram;
+
+namespace repo_man.xunit.domain.Diagram
+{
+    public class ConstantFileColorMapperTest
+    {
+        [Theory]
+        [InlineData(".cs", "#A377DA")]
+        [InlineData(".CS", "#A377DA")]  //handles different-case for file extension
+        [InlineData(".ts", "#3178C6")]
+        [InlineData(".js", "#F7E018")]
+        [InlineData(".json", "#A1A1A1")]
+        [InlineData(".md", "#FFFFFF")]
+        [InlineData(".sln", "#FF0000")]
+        [InlineData(".csproj", "#5988C6")]
+        [InlineData("codeowners","#8E5E3C")]
+        [InlineData(".gitignore", "#F54D27")] 
+        public void Returns_Constant_Color_Values_For_Common_File_Types(string extension, string expectedColor)
+        {
+            var mocker = new AutoMocker();
+            IFileColorMapper target = mocker.CreateInstance<ConstantFileColorMapper>();
+
+            target.Map(extension).Should().Be(expectedColor);
+        }
+    }
+
+    public class ConstantFileColorMapper : IFileColorMapper
+    {
+        private readonly Dictionary<string, string> _wellKnownFileTypes = new()
+        {
+            { ".cs", "#A377DA" },   //csharp purple
+            { ".sln", "#FF0000" },   //sln red
+            { ".csproj", "#5988C6" },   //csproj slate blue
+
+            { ".js", "#F7E018" },   //javascript yellow
+            { ".ts", "#3178C6" },   //typescript blue
+            { ".json", "#A1A1A1" },   //json grey
+            { ".md", "#FFFFFF" },   //white
+            { "codeowners", "#8E5E3C" },   //codeowners brown
+            {".gitignore", "#F54D27"}   //git orange
+        };
+
+        public string Map(string fileExtension)
+        {
+            return _wellKnownFileTypes[fileExtension.ToLowerInvariant()];
+        }
+    }
+}
