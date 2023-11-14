dcf1a73140b3738833796f002633e7fa15508743
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-11-14 07:48:17 -0600

    TDDing a file radius calculator we can work with

---
+++ src/repo-man.domain/Diagram/UnboundedFileRadiusCalculator.cs
@@ -0,0 +1,20 @@
+﻿using repo_man.domain.Git;
+
+namespace repo_man.domain.Diagram;
+
+public class UnboundedFileRadiusCalculator
+{
+    private readonly long _minFileSize;
+
+    public UnboundedFileRadiusCalculator(GitTree tree)
+    {
+        _minFileSize = tree.GetMinFileSize();
+    }
+
+    public int CalculateFileRadius(GitFile file)
+    {
+        const int minRadius = 10;
+        var radius = (int)(file.FileSize / _minFileSize) * minRadius;
+        return radius;
+    }
+}
---
+++ test/repo-man.xunit/domain/Diagram/UnboundedFileRadiusCalculatorTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using repo_man.domain.Diagram;
+using repo_man.domain.Git;
+using repo_man.xunit._helpers;
+
+namespace repo_man.xunit.domain.Diagram
+{
+    public class UnboundedFileRadiusCalculatorTest: TestBase
+    {
+        [Fact]
+        public void Returns10ForMinFileSize()
+        {
+            var minFileSize = _fixture.Create<long>();
+
+            var tree = new GitTree();
+            tree.AddFile("Fred.cs", minFileSize, Array.Empty<Commit>());
+
+            var target = new UnboundedFileRadiusCalculator(tree);
+
+            target.CalculateFileRadius(tree.Files.Single()).Should().Be(10);
+        }
+
+        [Fact]
+        public void Returns20IfFileTwiceAsLargeAsMin()
+        {
+            var minFileSize = Random.Shared.NextInt64(10L, Int16.MaxValue); //keep values conservative to prevent overflow
+
+            var tree = new GitTree();
+            tree.AddFile("Fred.cs", minFileSize, Array.Empty<Commit>());
+            tree.AddFile("Program.cs", minFileSize * 2, Array.Empty<Commit>());
+
+            var target = new UnboundedFileRadiusCalculator(tree);
+
+            target.CalculateFileRadius(tree.Files.Last()).Should().Be(20);
+        }
+    }
+}
