5519982dfb346a7b34507d63d2900ab578903a4e
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-11-16 07:51:17 -0600

    adding code comment and extracting interface to its own file

---
+++ src/repo-man.domain/Diagram/IFileRadiusCalculator.cs
@@ -0,0 +1,8 @@
+﻿using repo_man.domain.Git;
+
+namespace repo_man.domain.Diagram;
+
+public interface IFileRadiusCalculator
+{
+    int CalculateFileRadius(GitFile file, GitTree gitTree);
+}
--- src/repo-man.domain/Diagram/UnboundedFileRadiusCalculator.cs
+++ src/repo-man.domain/Diagram/UnboundedFileRadiusCalculator.cs
@@ -2,11 +2,10 @@
 
 namespace repo_man.domain.Diagram;
 
-public interface IFileRadiusCalculator
-{
-    int CalculateFileRadius(GitFile file, GitTree gitTree);
-}
-
+/// <summary>
+/// Calculates the radius of the circle based on the file size. The smallest file in the repository gets a radius of 10. All other files get a proportional radius.
+/// The limit to the maximum radius size is unbounded, hence the class name. 
+/// </summary>
 public class UnboundedFileRadiusCalculator : IFileRadiusCalculator
 {
     public int CalculateFileRadius(GitFile file, GitTree gitTree)
