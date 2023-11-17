444cfed96ca03fc01cb22b15e83aa37c15393ae0
kerry-patrick-il <kerry.patrick@imaginelearning.com>
2023-11-17 08:22:03 -0600

    refactor: moving new calculator to domain project

--- test/repo-man.xunit/domain/Diagram/BoundedFileRadiusCalculatorTest.cs
+++ test/repo-man.xunit/domain/Diagram/BoundedFileRadiusCalculatorTest.cs
@@ -135,41 +135,4 @@ namespace repo_man.xunit.domain.Diagram
         }
 
     }
-
-    public class BoundedFileRadiusCalculator : IFileRadiusCalculator
-    {
-        private readonly IConfiguration _config;
-
-        public BoundedFileRadiusCalculator(IConfiguration config)
-        {
-            _config = config;
-        }
-
-        public int CalculateFileRadius(GitFile file, GitTree gitTree)
-        {
-            const int minRadius = 10;
-            var maxRadius = 100;
-            if (_config["maxRadius"] is { } maxRadiusString 
-                && int.TryParse(maxRadiusString, out var newMaxRadius) 
-                && newMaxRadius >= minRadius)
-            {
-                maxRadius = newMaxRadius;
-            }
-
-            var minFileSize = gitTree.GetMinFileSize();
-            var maxFileSize = gitTree.GetMaxFileSize();
-
-            if (file.FileSize == minFileSize)
-            {
-                return minRadius;
-            }
-
-            //NOTE: should never get a divide by zero exception due to check above
-            var percent = (double)(file.FileSize - minFileSize) / (maxFileSize - minFileSize);
-            var increment = (int)Math.Round(percent * (maxRadius - minRadius));
-            var fileRadius = minRadius + increment;
-
-            return fileRadius;
-        }
-    }
 }
