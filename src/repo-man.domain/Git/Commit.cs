6e9d7f2a9cbf2bd222c7f3cef47fdd873bbefbe4
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-10-28 14:04:21 -0500

    Converting record to class so we can incrementally set properties

--- src/repo-man.domain/Git/Commit.cs
+++ src/repo-man.domain/Git/Commit.cs
@@ -1,3 +1,22 @@
 ﻿namespace repo_man.domain.Git;
 
-public record Commit(string Hash, DateTimeOffset CommitDate, string Author, string Message);
+public class Commit
+{
+    public Commit(string hash)
+    {
+        Hash = hash;
+    }
+
+    public Commit(string commitSha, DateTimeOffset committerWhen, string authorName, string commitMessage)
+    {
+        Hash = commitSha;
+        CommitDate = committerWhen;
+        Author = authorName;
+        Message = commitMessage;
+    }
+
+    public string Hash { get; set; }
+    public DateTimeOffset? CommitDate { get; set; }
+    public string? Author { get; set; }
+    public string? Message { get; set; }
+}
